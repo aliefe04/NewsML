45b352ed7fa3923aa0ed5421b820a1bd78b07499
aliefe04 <aliefe.cakici@std.yeditepe.edu.tr>
2022-12-13 20:40:09 +0300

    new

--- tools/textReviser.py
+++ tools/textReviser.py
@@ -2,13 +2,16 @@ import nltk
 import pyperclip
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-nltk.download('stopwords')
-nltk.download('punkt')
+from tkinter import Tk
+
+nltk.download("stopwords")
+nltk.download("punkt")
 # add punctuation to stopwords
-stopwords = stopwords.words('turkish')
+stopwords = stopwords.words("turkish")
+
 
 def textRev(filename):
-    text = open(f"{filename}.txt", "r", encoding="utf-8").read()
+    text = Tk().clipboard_get()
     text_tokens = word_tokenize(text)
     tokens_without_sw = [word for word in text_tokens if not word in stopwords]
     open(f"{filename}Rev.txt", "w", encoding="utf-8").write(" ".join(tokens_without_sw))
